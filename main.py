0869d380f49269fb18b0f77224370bc2ca23c6c4
Félix Ernesto Charry Pastrana <feecharrypa@unal.edu.co>
2021-07-11 14:54:52 -0500

    file to read, create model chosen on exploratoryTemplate.ipynb - Resumen.ipynb and save the results.

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import requests
+import scipy
+from scipy.optimize import curve_fit
+import random
+from scipy.stats.stats import pearsonr
+from sklearn.svm import SVR
+
